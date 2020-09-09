bde39a1dc1f9899ec91e6f2b519881b12de56452
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2020-09-09 11:57:58 +1000

    Dividend paying token contract WIP

--- contracts/DividendPayingToken.sol
+++ contracts/DividendPayingToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.7.0;
 
 // ----------------------------------------------------------------------------
 // Dividend Paying Token
@@ -6,88 +6,88 @@ pragma solidity ^0.5.0;
 // NOTE: This token contract allows the owner to mint and burn tokens for any
 // account, and is used for testing
 //
-// Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2019.
+// Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2019 of the Optino Project
 //
-// GNU Lesser General Public License 3.0
-// https://www.gnu.org/licenses/lgpl-3.0.en.html
+// SPDX-License-Identifier: MIT
 // ----------------------------------------------------------------------------
 
 // import "Owned.sol";
-// ----------------------------------------------------------------------------
-// Owned contract
-// ----------------------------------------------------------------------------
+/// @notice Ownership
 contract Owned {
+    bool initialised;
     address public owner;
     address public newOwner;
 
     event OwnershipTransferred(address indexed _from, address indexed _to);
 
     modifier onlyOwner {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Not owner");
         _;
     }
 
     function initOwned(address _owner) internal {
-        owner = _owner;
+        require(!initialised, "Already initialised");
+        owner = address(uint160(_owner));
+        initialised = true;
     }
     function transferOwnership(address _newOwner) public onlyOwner {
         newOwner = _newOwner;
     }
     function acceptOwnership() public {
-        require(msg.sender == newOwner);
         emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
         newOwner = address(0);
     }
-    function transferOwnershipImmediately(address _newOwner) public onlyOwner {
-        emit OwnershipTransferred(owner, _newOwner);
-        owner = _newOwner;
-    }
 }
 
+
 // import "SafeMath.sol";
-// ----------------------------------------------------------------------------
-// Safe maths
-// ----------------------------------------------------------------------------
+/// @notice Safe maths
 library SafeMath {
     function add(uint a, uint b) internal pure returns (uint c) {
         c = a + b;
-        require(c >= a);
+        require(c >= a, "Add overflow");
     }
     function sub(uint a, uint b) internal pure returns (uint c) {
-        require(b <= a);
+        require(b <= a, "Sub underflow");
         c = a - b;
     }
     function mul(uint a, uint b) internal pure returns (uint c) {
         c = a * b;
-        require(a == 0 || c / a == b);
+        require(a == 0 || c / a == b, "Mul overflow");
     }
     function div(uint a, uint b) internal pure returns (uint c) {
-        require(b > 0);
+        require(b > 0, "Divide by 0");
         c = a / b;
-    }
-    function max(uint a, uint b) internal pure returns (uint c) {
-        c = a >= b ? a : b;
-    }
-    function min(uint a, uint b) internal pure returns (uint c) {
-        c = a <= b ? a : b;
     }
 }
 
-// import "ApproveAndCallFallback.sol";
+
+/// @notice ERC20 https://eips.ethereum.org/EIPS/eip-20 with optional symbol, name and decimals
+interface ERC20 {
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+
+    function symbol() external view returns (string memory);
+    function name() external view returns (string memory);
+    function decimals() external view returns (uint8);
+}
 
 // import "MintableTokenInterface.sol";
 // ----------------------------------------------------------------------------
 // MintableToken Interface = ERC20 + symbol + name + decimals + mint + burn
 // + approveAndCall
 // ----------------------------------------------------------------------------
-contract MintableTokenInterface is ERC20Interface {
-    function symbol() public view returns (string memory);
-    function name() public view returns (string memory);
-    function decimals() public view returns (uint8);
-    function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success);
-    function mint(address tokenOwner, uint tokens) public returns (bool success);
-    function burn(address tokenOwner, uint tokens) public returns (bool success);
+interface MintableToken is ERC20 {
+    function mint(address tokenOwner, uint tokens) external returns (bool success);
+    function burn(address tokenOwner, uint tokens) external returns (bool success);
 }
 
 
@@ -98,7 +98,7 @@ contract MintableTokenInterface is ERC20Interface {
 // NOTE: This token contract allows the owner to mint and burn tokens for any
 // account, and is used for testing
 // ----------------------------------------------------------------------------
-contract DividendPayingToken is MintableTokenInterface, Owned {
+contract DividendPayingToken is MintableToken, Owned {
     using SafeMath for uint;
 
     string _symbol;
@@ -171,15 +171,15 @@ function disburse(uint amount) {
         _unclaimedDividends = _unclaimedDividends.add(dividends);
         emit LogInfo("depositDividends: _totalDividendPoints", _totalDividendPoints, 0x0, "", address(0));
         emit LogInfo("depositDividends: _unclaimedDividends", _unclaimedDividends, 0x0, "", address(0));
-        ERC20Interface(token).transferFrom(msg.sender, address(this), dividends);
+        ERC20(token).transferFrom(msg.sender, address(this), dividends);
     }
 
-    function withdrawDividends(address token) public returns (uint withdrawn) {
+    function withdrawDividends(address /*token*/) public returns (uint withdrawn) {
         updateAccount(msg.sender);
         withdrawn = 0;
     }
 
-    constructor(string memory symbol, string memory name, uint8 decimals, address tokenOwner, uint initialSupply, address _dividendToken) public {
+    constructor(string memory symbol, string memory name, uint8 decimals, address tokenOwner, uint initialSupply, address _dividendToken) {
         initOwned(msg.sender);
         _symbol = symbol;
         _name = name;
@@ -189,22 +189,22 @@ function disburse(uint amount) {
         emit Transfer(address(0), tokenOwner, _totalSupply);
         dividendToken = _dividendToken;
     }
-    function symbol() public view returns (string memory) {
+    function symbol() override external view returns (string memory) {
         return _symbol;
     }
-    function name() public view returns (string memory) {
+    function name() override external view returns (string memory) {
         return _name;
     }
-    function decimals() public view returns (uint8) {
+    function decimals() override external view returns (uint8) {
         return _decimals;
     }
-    function totalSupply() public view returns (uint) {
+    function totalSupply() override external view returns (uint) {
         return _totalSupply.sub(accounts[address(0)].balance);
     }
-    function balanceOf(address tokenOwner) public view returns (uint balance) {
+    function balanceOf(address tokenOwner) override external view returns (uint balance) {
         return accounts[tokenOwner].balance;
     }
-    function transfer(address to, uint tokens) public returns (bool success) {
+    function transfer(address to, uint tokens) override external returns (bool success) {
         updateAccount(msg.sender);
         updateAccount(to);
         accounts[msg.sender].balance = accounts[msg.sender].balance.sub(tokens);
@@ -212,12 +212,12 @@ function disburse(uint amount) {
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
-    function approve(address spender, uint tokens) public returns (bool success) {
+    function approve(address spender, uint tokens) override external returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+    function transferFrom(address from, address to, uint tokens) override external returns (bool success) {
         updateAccount(from);
         updateAccount(to);
         accounts[from].balance = accounts[from].balance.sub(tokens);
@@ -226,22 +226,16 @@ function disburse(uint amount) {
         emit Transfer(from, to, tokens);
         return true;
     }
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+    function allowance(address tokenOwner, address spender) override external view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
-    function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallback(spender).receiveApproval(msg.sender, tokens, address(this), data);
-        return true;
-    }
-    function mint(address tokenOwner, uint tokens) public onlyOwner returns (bool success) {
+    function mint(address tokenOwner, uint tokens) override external onlyOwner returns (bool success) {
         accounts[tokenOwner].balance = accounts[tokenOwner].balance.add(tokens);
         _totalSupply = _totalSupply.add(tokens);
         emit Transfer(address(0), tokenOwner, tokens);
         return true;
     }
-    function burn(address tokenOwner, uint tokens) public onlyOwner returns (bool success) {
+    function burn(address tokenOwner, uint tokens) override external onlyOwner returns (bool success) {
         accounts[tokenOwner].balance = accounts[tokenOwner].balance.sub(tokens);
         _totalSupply = _totalSupply.sub(tokens);
         emit Transfer(tokenOwner, address(0), tokens);
---
+++ contracts/Owned.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.7.0;
+
+/// @notice Ownership
+contract Owned {
+    bool initialised;
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function initOwned(address _owner) internal {
+        require(!initialised, "Already initialised");
+        owner = address(uint160(_owner));
+        initialised = true;
+    }
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
