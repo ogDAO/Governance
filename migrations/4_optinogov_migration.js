d84cfab274c37ebceffa19acd62cde1f4a20d7ab
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2020-09-23 04:02:41 +1000

    Migrations

--- migrations/4_optinogov_migration.js
+++ migrations/4_optinogov_migration.js
@@ -3,13 +3,10 @@ const BigNumber = require('bignumber.js');
 var OFToken = artifacts.require("OFToken");
 var OGToken = artifacts.require("OGToken");
 var OptinoGov = artifacts.require("OptinoGov");
-var POAPOGTokenStation = artifacts.require("POAPOGTokenStation");
-var TestToken = artifacts.require("TestToken");
+// var POAPOGTokenStation = artifacts.require("POAPOGTokenStation");
 
 module.exports = function(deployer, network, accounts) {
   let owner = accounts[0];
-  // console.log('deployer: ' + deployer);
-  // deployer.deploy(TestToken, "ABC", "Abc", 18, tokenOwner, new web3.utils.BN("1000"), {from: deployer});
+  // console.log('owner: ' + owner);
   deployer.deploy(OptinoGov, OGToken.address, { from: owner, gas: 5000000 });
-
 };
--- migrations/5_testtoken_migration.js
+++ migrations/5_testtoken_migration.js
@@ -1,16 +1,9 @@
 const BigNumber = require('bignumber.js');
 
-var OFToken = artifacts.require("OFToken");
-var OGToken = artifacts.require("OGToken");
-var OptinoGov = artifacts.require("OptinoGov");
-var POAPOGTokenStation = artifacts.require("POAPOGTokenStation");
 var TestToken = artifacts.require("TestToken");
 
 module.exports = function(deployer, network, accounts) {
   let owner = accounts[0];
-  // console.log('deployer: ' + deployer);
-  // deployer.deploy(TestToken, "ABC", "Abc", 18, tokenOwner, new web3.utils.BN("1000"), {from: deployer});
-  deployer.deploy(TestToken, "ABC", "Abc", 18, owner, new BigNumber("1000000").shiftedBy(18), { from: owner, gas: 2000000 });
-
-// string memory symbol, string memory name, uint8 decimals, address tokenOwner, uint initialSupply
+  // console.log('owner: ' + owner);
+  deployer.deploy(TestToken, "TEST", "Test", 18, owner, new BigNumber("1000000").shiftedBy(18), { from: owner, gas: 2000000 });
 };
