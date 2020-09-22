fe5f31f8a24f0fb39337c5917d72a4dab3ddc038
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2020-09-23 03:40:22 +1000

    Permissioned in separate file

--- contracts/OGToken.sol
+++ contracts/OGToken.sol
@@ -1,61 +1,20 @@
 pragma solidity ^0.7.0;
 
-// import "https://github.com/ogDAO/Governance/blob/master/contracts/Owned.sol";
-// import "https://github.com/ogDAO/Governance/blob/master/contracts/SafeMath.sol";
+// import "https://github.com/ogDAO/Governance/blob/master/contracts/Permissioned.sol";
 // import "https://github.com/ogDAO/Governance/blob/master/contracts/OGTokenInterface.sol";
 
-import "./Owned.sol";
-import "./SafeMath.sol";
+import "./Permissioned.sol";
 import "./OGTokenInterface.sol";
+
 
 // ----------------------------------------------------------------------------
 // Optino Governance Token
 //
+// OGToken = OGTokenInterface (ERC20 + mint + burn) + dividend payment
+//
 // Enjoy. (c) The Optino Project 2020
 //
 // SPDX-License-Identifier: GPLv2
-// ----------------------------------------------------------------------------
-
-// import "Permissioned.sol";
-/// @notice Permissioned
-contract Permissioned is Owned {
-    using SafeMath for uint;
-
-    struct Permission {
-        bool active;
-        uint maximum;
-        uint processed;
-    }
-
-    uint public constant ROLE_MINTER = 1;
-    // Don't need ROLE_BURNER at the moment
-    // uint public constant ROLE_BURNER = 2;
-    mapping(address => mapping(uint => Permission)) public permissions;
-
-    modifier permitted(uint role, uint tokens) {
-        Permission storage permission = permissions[msg.sender][role];
-        require(permission.active && (permission.maximum == 0 || permission.processed + tokens < permission.maximum), "Not permissioned");
-        permission.processed = permission.processed.add(tokens);
-        _;
-    }
-
-    function initPermissioned(address _owner) internal {
-        initOwned(_owner);
-        setPermission(_owner, ROLE_MINTER, true, 0);
-        // setPermission(_owner, ROLE_BURNER, true, 0);
-    }
-    function setPermission(address account, uint role, bool active, uint maximum) public {
-        uint processed = permissions[account][role].processed;
-        permissions[account][role] = Permission({ active: active, maximum: maximum, processed: processed });
-    }
-    function processed(uint role, uint tokens) internal {
-        permissions[msg.sender][role].processed = permissions[msg.sender][role].processed.add(tokens);
-    }
-}
-
-
-// ----------------------------------------------------------------------------
-// OGToken = OGTokenInterface (ERC20 + mint + burn) + dividend payment
 // ----------------------------------------------------------------------------
 contract OGToken is OGTokenInterface, Permissioned {
     using SafeMath for uint;
---
+++ contracts/Permissioned.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.7.0;
+
+import "./Owned.sol";
+import "./SafeMath.sol";
+
+/// @notice Permissioned
+// SPDX-License-Identifier: GPLv2
+contract Permissioned is Owned {
+    using SafeMath for uint;
+
+    struct Permission {
+        bool active;
+        uint maximum;
+        uint processed;
+    }
+
+    uint public constant ROLE_MINTER = 1;
+    // Don't need ROLE_BURNER at the moment
+    // uint public constant ROLE_BURNER = 2;
+    mapping(address => mapping(uint => Permission)) public permissions;
+
+    modifier permitted(uint role, uint tokens) {
+        Permission storage permission = permissions[msg.sender][role];
+        require(permission.active && (permission.maximum == 0 || permission.processed + tokens < permission.maximum), "Not permissioned");
+        permission.processed = permission.processed.add(tokens);
+        _;
+    }
+
+    function initPermissioned(address _owner) internal {
+        initOwned(_owner);
+        setPermission(_owner, ROLE_MINTER, true, 0);
+        // setPermission(_owner, ROLE_BURNER, true, 0);
+    }
+    function setPermission(address account, uint role, bool active, uint maximum) public onlyOwner {
+        uint processed = permissions[account][role].processed;
+        permissions[account][role] = Permission({ active: active, maximum: maximum, processed: processed });
+    }
+    function processed(uint role, uint tokens) internal {
+        permissions[msg.sender][role].processed = permissions[msg.sender][role].processed.add(tokens);
+    }
+}
