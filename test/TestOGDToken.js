c36bff822c7250d0be6096002be046aafda707df
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2020-10-22 15:37:11 +1100

    Tidy

--- test/TestOGDToken.js
+++ test/TestOGDToken.js
@@ -15,7 +15,7 @@ describe("TestOGDToken", function() {
     await data.init();
 
     console.log("        --- Setup 1 - Deploy OGDToken, FEE{0..2} ---");
-    var setup1 = [];
+    const setup1 = [];
     setup1.push(OGDToken.deploy("OGD", "Optino Governance Dividend", 18, data.owner, new BigNumber("0").shiftedBy(18).toFixed(0)));
     setup1.push(TestToken.deploy("FEE0", "Fee0", 18, data.owner, new BigNumber("10000").shiftedBy(18).toFixed(0)));
     setup1.push(TestToken.deploy("FEE1", "Fee1", 18, data.owner, new BigNumber("10000").shiftedBy(18).toFixed(0)));
@@ -29,11 +29,10 @@ describe("TestOGDToken", function() {
     await data.printBalances();
 
     console.log("        --- Setup 2 - OGDToken.addDividendTokens for ETH and FEE0, OGDToken mint(...) permissioning ---");
-    var setup2 = [];
-    var ogdTokens = new BigNumber("10000").shiftedBy(18);
-    setup2.push(ogdToken.addDividendToken(ZERO_ADDRESS));
-    setup2.push(ogdToken.addDividendToken(fee0Token.address));
-    setup2.push(ogdToken.setPermission(data.owner, 1, true, 0));
+    const setup2 = [];
+    setup2.push(data.ogdToken.addDividendToken(ZERO_ADDRESS));
+    setup2.push(data.ogdToken.addDividendToken(data.fee0Token.address));
+    setup2.push(data.ogdToken.setPermission(data.owner, 1, true, 0));
     const [addDividendToken0, addDividendToken1, setPermission1] = await Promise.all(setup2);
     data.printTxData("addDividendToken0", addDividendToken0);
     data.printTxData("addDividendToken1", addDividendToken1);
@@ -41,17 +40,17 @@ describe("TestOGDToken", function() {
     await data.printBalances();
 
     console.log("        --- Setup 3 - Mint 10,000 OGD tokens for User{1..3}; Owner approve 100 FEE for OGToken to spend ---");
-    var setup3 = [];
-    var ogdTokens = new BigNumber("10000").shiftedBy(18);
-    var approveFee0Tokens = new BigNumber("100").shiftedBy(18);
-    var approveFee1Tokens = new BigNumber("1000").shiftedBy(18);
-    var approveFee2Tokens = new BigNumber("10000").shiftedBy(18);
-    setup3.push(ogdToken.mint(data.user1, ogdTokens.toFixed(0)));
-    setup3.push(ogdToken.mint(data.user2, ogdTokens.toFixed(0)));
-    setup3.push(ogdToken.mint(data.user3, ogdTokens.toFixed(0)));
-    setup3.push(fee0Token.approve(ogdToken.address, approveFee0Tokens.toFixed(0)));
-    setup3.push(fee1Token.approve(ogdToken.address, approveFee1Tokens.toFixed(0)));
-    setup3.push(fee2Token.approve(ogdToken.address, approveFee2Tokens.toFixed(0)));
+    const setup3 = [];
+    const ogdTokens = new BigNumber("10000").shiftedBy(18);
+    const approveFee0Tokens = new BigNumber("100").shiftedBy(18);
+    const approveFee1Tokens = new BigNumber("1000").shiftedBy(18);
+    const approveFee2Tokens = new BigNumber("10000").shiftedBy(18);
+    setup3.push(data.ogdToken.mint(data.user1, ogdTokens.toFixed(0)));
+    setup3.push(data.ogdToken.mint(data.user2, ogdTokens.toFixed(0)));
+    setup3.push(data.ogdToken.mint(data.user3, ogdTokens.toFixed(0)));
+    setup3.push(data.fee0Token.approve(data.ogdToken.address, approveFee0Tokens.toFixed(0)));
+    setup3.push(data.fee1Token.approve(data.ogdToken.address, approveFee1Tokens.toFixed(0)));
+    setup3.push(data.fee2Token.approve(data.ogdToken.address, approveFee2Tokens.toFixed(0)));
     const [mint1, mint2, mint3, ownerApproveFee0Tokens, ownerApproveFee1Tokens, ownerApproveFee2Tokens] = await Promise.all(setup3);
     await data.printTxData("mint1", mint1);
     await data.printTxData("mint2", mint2);
@@ -69,94 +68,105 @@ describe("TestOGDToken", function() {
   // it("Standard Workflow #0", async function() {
   // });
 
-  it("Standard Workflow #1", async function() {
-    const [ogdToken, fee0Token, fee1Token, fee2Token] = [data.ogdToken, data.fee0Token, data.fee1Token, data.fee2Token];
-    const [user1Signer, user2Signer, user3Signer] = [data.user1Signer, data.user2Signer, data.user3Signer];
+  describe("TestOGDToken - Standard Workflow #0", function() {
+    it("Standard Workflow #0", async function() {
+      console.log("Standard Workflow #0");
+    });
+  });
 
-    console.log("        --- Test 1 - Owner deposits dividends of 100 FEE0 and 10 ETH ---");
-    var test1 = [];
-    var depositFee0Tokens = new BigNumber("100").shiftedBy(18);
-    var depositFeeETH = new BigNumber("10").shiftedBy(18);
-    test1.push(ogdToken.depositDividend(fee0Token.address, depositFee0Tokens.toFixed(0)));
-    test1.push(ogdToken.depositDividend(ZERO_ADDRESS, depositFeeETH.toFixed(0), { value: depositFeeETH.toFixed(0) }));
-    const [depositDividendFee0, depositDividendETH0] = await Promise.all(test1);
-    await data.printTxData("depositDividendFee0", depositDividendFee0);
-    await data.printTxData("depositDividendETH0", depositDividendETH0);
-    await data.printBalances();
+  describe("TestOGDToken - Standard Workflow #1", function() {
+    it("Standard Workflow #1", async function() {
+      console.log("Standard Workflow #1");
+    });
+  });
 
-    console.log("        --- Test 2 - User1 dummy transfer to same account (internal stats update) ---");
-    var test2 = [];
-    test2.push(ogdToken.connect(user2Signer).transfer(data.user2, "1"));
-    const [transfer1] = await Promise.all(test2);
-    await data.printTxData("transfer1", transfer1);
-    await data.printBalances();
+  describe.only("TestOGDToken - Standard Workflow #2", function() {
+    it("Standard Workflow #2", async function() {
+      console.log("        --- Test 1 - Owner deposits dividends of 100 FEE0 and 10 ETH ---");
+      const test1 = [];
+      const depositFee0Tokens = new BigNumber("100").shiftedBy(18);
+      const depositFeeETH = new BigNumber("10").shiftedBy(18);
+      test1.push(data.ogdToken.depositDividend(data.fee0Token.address, depositFee0Tokens.toFixed(0)));
+      test1.push(data.ogdToken.depositDividend(ZERO_ADDRESS, depositFeeETH.toFixed(0), { value: depositFeeETH.toFixed(0) }));
+      const [depositDividendFee0, depositDividendETH0] = await Promise.all(test1);
+      await data.printTxData("depositDividendFee0", depositDividendFee0);
+      await data.printTxData("depositDividendETH0", depositDividendETH0);
+      await data.printBalances();
 
-    console.log("        --- Test 3 - User{1..3} withdraw 33.333333333333333333 FEE0 and 3.333333333333333333 ETH ---");
-    var test3 = [];
-    test3.push(ogdToken.connect(user1Signer).withdrawDividends());
-    test3.push(ogdToken.connect(user2Signer).withdrawDividends());
-    test3.push(ogdToken.connect(user3Signer).withdrawDividends());
-    const [withdrawDividends1, withdrawDividends2, withdrawDividends3] = await Promise.all(test3);
-    await data.printTxData("withdrawDividends1", withdrawDividends1);
-    await data.printTxData("withdrawDividends2", withdrawDividends2);
-    await data.printTxData("withdrawDividends3", withdrawDividends3);
-    await data.printBalances();
+      console.log("        --- Test 2 - User1 dummy transfer to same account (internal stats update) ---");
+      const test2 = [];
+      test2.push(data.ogdToken.connect(data.user2Signer).transfer(data.user2, "1"));
+      const [transfer1] = await Promise.all(test2);
+      await data.printTxData("transfer1", transfer1);
+      await data.printBalances();
 
-    console.log("        --- Test 4 - Add Fee{1..2} Dividend Token ---");
-    var test4 = [];
-    test4.push(ogdToken.addDividendToken(fee1Token.address));
-    test4.push(ogdToken.addDividendToken(fee2Token.address));
-    const [addDividendToken2, addDividendToken3] = await Promise.all(test4);
-    await data.printTxData("addDividendToken2", addDividendToken2);
-    await data.printTxData("addDividendToken3", addDividendToken3);
-    await data.printBalances();
+      console.log("        --- Test 3 - User{1..3} withdraw 33.333333333333333333 FEE0 and 3.333333333333333333 ETH ---");
+      const test3 = [];
+      test3.push(data.ogdToken.connect(data.user1Signer).withdrawDividends());
+      test3.push(data.ogdToken.connect(data.user2Signer).withdrawDividends());
+      test3.push(data.ogdToken.connect(data.user3Signer).withdrawDividends());
+      const [withdrawDividends1, withdrawDividends2, withdrawDividends3] = await Promise.all(test3);
+      await data.printTxData("withdrawDividends1", withdrawDividends1);
+      await data.printTxData("withdrawDividends2", withdrawDividends2);
+      await data.printTxData("withdrawDividends3", withdrawDividends3);
+      await data.printBalances();
 
-    console.log("        --- Test 5 - Owner deposits dividends of 1,000 FEE1 and 10,000 FEE2 ---");
-    var test5 = [];
-    var depositFee1Tokens = new BigNumber("1000").shiftedBy(18);
-    var depositFee2Tokens = new BigNumber("10000").shiftedBy(18);
-    test5.push(ogdToken.depositDividend(fee1Token.address, depositFee1Tokens.toFixed(0)));
-    test5.push(ogdToken.depositDividend(fee2Token.address, depositFee2Tokens.toFixed(0)));
-    const [depositDividendFee1, depositDividendFee2] = await Promise.all(test5);
-    await data.printTxData("depositDividendFee1", depositDividendFee1);
-    await data.printTxData("depositDividendFee2", depositDividendFee2);
-    await data.printBalances();
+      console.log("        --- Test 4 - Add Fee{1..2} Dividend Token ---");
+      const test4 = [];
+      test4.push(data.ogdToken.addDividendToken(data.fee1Token.address));
+      test4.push(data.ogdToken.addDividendToken(data.fee2Token.address));
+      const [addDividendToken2, addDividendToken3] = await Promise.all(test4);
+      await data.printTxData("addDividendToken2", addDividendToken2);
+      await data.printTxData("addDividendToken3", addDividendToken3);
+      await data.printBalances();
 
-    console.log("        --- Test 6 - User{1..3} withdraw 333.333333333333333333 FEE1 and 3333.333333333333333333 FEE2 ---");
-    var test6 = [];
-    test6.push(ogdToken.connect(user1Signer).withdrawDividends());
-    test6.push(ogdToken.connect(user2Signer).withdrawDividends());
-    test6.push(ogdToken.connect(user3Signer).withdrawDividends());
-    const [withdrawDividends4, withdrawDividends5, withdrawDividends6] = await Promise.all(test6);
-    await data.printTxData("withdrawDividends4", withdrawDividends4);
-    await data.printTxData("withdrawDividends5", withdrawDividends5);
-    await data.printTxData("withdrawDividends6", withdrawDividends6);
-    await data.printBalances();
+      console.log("        --- Test 5 - Owner deposits dividends of 1,000 FEE1 and 10,000 FEE2 ---");
+      const test5 = [];
+      const depositFee1Tokens = new BigNumber("1000").shiftedBy(18);
+      const depositFee2Tokens = new BigNumber("10000").shiftedBy(18);
+      test5.push(data.ogdToken.depositDividend(data.fee1Token.address, depositFee1Tokens.toFixed(0)));
+      test5.push(data.ogdToken.depositDividend(data.fee2Token.address, depositFee2Tokens.toFixed(0)));
+      const [depositDividendFee1, depositDividendFee2] = await Promise.all(test5);
+      await data.printTxData("depositDividendFee1", depositDividendFee1);
+      await data.printTxData("depositDividendFee2", depositDividendFee2);
+      await data.printBalances();
 
-    console.log("        --- Test 7 - User2 transfer 1 OGD to User3 ---");
-    var test7 = [];
-    test7.push(ogdToken.connect(user2Signer).transfer(data.user3, new BigNumber("0.123456789123456789").shiftedBy(18).toFixed(0)));
-    const [transfer2] = await Promise.all(test7);
-    await data.printTxData("transfer2", transfer2);
-    await data.printBalances();
+      console.log("        --- Test 6 - User{1..3} withdraw 333.333333333333333333 FEE1 and 3333.333333333333333333 FEE2 ---");
+      const test6 = [];
+      test6.push(data.ogdToken.connect(data.user1Signer).withdrawDividends());
+      test6.push(data.ogdToken.connect(data.user2Signer).withdrawDividends());
+      test6.push(data.ogdToken.connect(data.user3Signer).withdrawDividends());
+      const [withdrawDividends4, withdrawDividends5, withdrawDividends6] = await Promise.all(test6);
+      await data.printTxData("withdrawDividends4", withdrawDividends4);
+      await data.printTxData("withdrawDividends5", withdrawDividends5);
+      await data.printTxData("withdrawDividends6", withdrawDividends6);
+      await data.printBalances();
 
-    const user1Fee1Balance = await fee1Token.balanceOf(data.user1);
-    console.log("        user1Fee1Balance: " + user1Fee1Balance);
-    expect(new BigNumber(user1Fee1Balance.toString()).toFixed(0)).to.equal(new BigNumber("333.333333333333333333").shiftedBy(18).toFixed(0));
+      console.log("        --- Test 7 - User2 transfer 1 OGD to User3 ---");
+      const test7 = [];
+      test7.push(data.ogdToken.connect(data.user2Signer).transfer(data.user3, new BigNumber("0.123456789123456789").shiftedBy(18).toFixed(0)));
+      const [transfer2] = await Promise.all(test7);
+      await data.printTxData("transfer2", transfer2);
+      await data.printBalances();
+
+      const user1Fee1Balance = await data.fee1Token.balanceOf(data.user1);
+      console.log("        user1Fee1Balance: " + user1Fee1Balance);
+      expect(new BigNumber(user1Fee1Balance.toString()).toFixed(0)).to.equal(new BigNumber("333.333333333333333333").shiftedBy(18).toFixed(0));
 
 
-    // const Greeter = await ethers.getContractFactory("Greeter");
-    // const greeter = await Greeter.deploy("Hello, world!");
-    //
-    // await greeter.deployed();
-    // expect(await greeter.greet()).to.equal("Hello, world!");
-    //
-    // await greeter.setGreeting("Hola, mundo!");
-    // // await greeter.connect(data.user1).setGreeting("Hola, mundo!");
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+      // const Greeter = await ethers.getContractFactory("Greeter");
+      // const greeter = await Greeter.deploy("Hello, world!");
+      //
+      // await greeter.deployed();
+      // expect(await greeter.greet()).to.equal("Hello, world!");
+      //
+      // await greeter.setGreeting("Hola, mundo!");
+      // // await greeter.connect(data.user1).setGreeting("Hola, mundo!");
+      // expect(await greeter.greet()).to.equal("Hola, mundo!");
 
-    // await ethers.provider.getLogs({}).then((data) => {
-    //   console.log("getLogs: " + util.inspect(data));
-    // });
+      // await ethers.provider.getLogs({}).then((data) => {
+      //   console.log("getLogs: " + util.inspect(data));
+      // });
+    });
   });
 });
--- test/TestOptinoGov.js
+++ test/TestOptinoGov.js
@@ -9,6 +9,7 @@ let TestToken;
 let OptinoGov;
 let data;
 
+// describe.only, describe.skip
 describe("TestOptinoGov", function() {
   beforeEach("Setup", async function() {
     OGToken = await ethers.getContractFactory("OGToken");
@@ -19,12 +20,12 @@ describe("TestOptinoGov", function() {
     await data.init();
 
     console.log("        --- Setup 1 - Deploy OGToken, OGDToken, FEE0, then OptinoGov ---");
-    var setup1a = [];
+    const setup1a = [];
     setup1a.push(OGToken.deploy("OG", "Optino Governance", 18, data.owner, new BigNumber("40000").shiftedBy(18).toFixed(0)));
     setup1a.push(OGDToken.deploy("OGD", "Optino Governance Dividend", 18, data.owner, new BigNumber("0").shiftedBy(18).toFixed(0)));
     setup1a.push(TestToken.deploy("FEE0", "Fee0", 18, data.owner, new BigNumber("1000").shiftedBy(18).toFixed(0)));
     const [ogToken, ogdToken, fee0Token] = await Promise.all(setup1a);
-    var setup1b = [];
+    const setup1b = [];
     setup1b.push(OptinoGov.deploy(ogToken.address, ogdToken.address));
     const [optinoGov] = await Promise.all(setup1b);
     await data.setOptinoGovData(ogToken, ogdToken, fee0Token, optinoGov);
@@ -35,7 +36,7 @@ describe("TestOptinoGov", function() {
     // await data.printBalances();
 
     console.log("        --- Setup 2 - OGDToken.addDividendToken([ETH, FEE0]), OGDToken mint(...) permissioning ---");
-    var setup2 = [];
+    const setup2 = [];
     setup2.push(data.ogdToken.addDividendToken(ZERO_ADDRESS));
     setup2.push(data.ogdToken.addDividendToken(fee0Token.address));
     setup2.push(ogToken.setPermission(data.optinoGov.address, 1, true, 0));
@@ -48,9 +49,9 @@ describe("TestOptinoGov", function() {
     // await data.printBalances();
 
     console.log("        --- Setup 3 - Transfer OGTokens, User{1..3} approve 2,000 OGTokens to OptinoGov, Owner approve 2,000 FEE0 tokens to OGDToken, Transfer OGToken and OGDToken ownership to OptinoGov ---");
-    var ogTokens = new BigNumber("10000").shiftedBy(18);
-    var approveTokens = new BigNumber("2000").shiftedBy(18);
-    var setup3 = [];
+    const ogTokens = new BigNumber("10000").shiftedBy(18);
+    const approveTokens = new BigNumber("2000").shiftedBy(18);
+    const setup3 = [];
     setup3.push(ogToken.transfer(data.user1, ogTokens.toFixed(0)));
     setup3.push(ogToken.transfer(data.user2, ogTokens.toFixed(0)));
     setup3.push(ogToken.transfer(data.user3, ogTokens.toFixed(0)));
@@ -77,101 +78,107 @@ describe("TestOptinoGov", function() {
   //      console.log("Event: " + result);
   //    });
   });
-  // it("Standard Workflow #0", async function() {
-  // });
 
-  // it("Standard Workflow #1", async function() {
-  // });
+  describe("TestOptinoGov - Standard Workflow #0", function() {
+    it("Standard Workflow #0", async function() {
+      console.log("Standard Workflow #0");
+    });
+  });
 
-  it("Standard Workflow #2", async function() {
-    // Remove after development
-    // return;
+  describe("TestOptinoGov - Standard Workflow #1", function() {
+    it("Standard Workflow #1", async function() {
+      console.log("Standard Workflow #1");
+    });
+  });
 
-    console.log("        --- Test 1 - User{1,2} delegate to User3---");
-    var test1 = [];
-    test1.push(data.optinoGov.connect(data.user1Signer).delegate(data.user3));
-    test1.push(data.optinoGov.connect(data.user2Signer).delegate(data.user3));
-    const [delegate1, delegate2] = await Promise.all(test1);
-    await data.printTxData("delegate1", delegate1);
-    await data.printTxData("delegate2", delegate2);
-    await data.printBalances();
+  describe.only("TestOptinoGov - Standard Workflow #2", function() {
+    it("Standard Workflow #2", async function() {
+      console.log("        --- Test 1 - User{1,2} delegate to User3---");
+      const test1 = [];
+      test1.push(data.optinoGov.connect(data.user1Signer).delegate(data.user3));
+      test1.push(data.optinoGov.connect(data.user2Signer).delegate(data.user3));
+      const [delegate1, delegate2] = await Promise.all(test1);
+      await data.printTxData("delegate1", delegate1);
+      await data.printTxData("delegate2", delegate2);
+      await data.printBalances();
 
-    console.log("        --- Test 2 - User{1..3} commit OGTokens for {5, 50, 500} seconds duration ---");
-    var lockTokens = new BigNumber("1000").shiftedBy(18);
-    var test2 = [];
-    test2.push(data.optinoGov.connect(data.user1Signer).commit(lockTokens.toFixed(0), 5));
-    test2.push(data.optinoGov.connect(data.user2Signer).commit(lockTokens.toFixed(0), 50));
-    test2.push(data.optinoGov.connect(data.user3Signer).commit(lockTokens.toFixed(0), 500));
-    const [commit1, commit2, commit3] = await Promise.all(test2);
-    await data.printTxData("commit1", commit1);
-    await data.printTxData("commit2", commit2);
-    await data.printTxData("commit3", commit3);
-    await data.printBalances();
+      console.log("        --- Test 2 - User{1..3} commit OGTokens for {5, 50, 500} seconds duration ---");
+      const lockTokens = new BigNumber("1000").shiftedBy(18);
+      const test2 = [];
+      test2.push(data.optinoGov.connect(data.user1Signer).commit(lockTokens.toFixed(0), 5));
+      test2.push(data.optinoGov.connect(data.user2Signer).commit(lockTokens.toFixed(0), 50));
+      test2.push(data.optinoGov.connect(data.user3Signer).commit(lockTokens.toFixed(0), 500));
+      const [commit1, commit2, commit3] = await Promise.all(test2);
+      await data.printTxData("commit1", commit1);
+      await data.printTxData("commit2", commit2);
+      await data.printTxData("commit3", commit3);
+      await data.printBalances();
 
-    console.log("        --- Test 3 - User{2} commit again for {55} seconds duration ---");
-    var test3 = [];
-    test3.push(data.optinoGov.connect(data.user2Signer).commit(lockTokens.toFixed(0), 55));
-    const [commit4] = await Promise.all(test3);
-    await data.printTxData("commit4", commit4);
-    await data.printBalances();
+      console.log("        --- Test 3 - User{2} commit again for {55} seconds duration ---");
+      const test3 = [];
+      test3.push(data.optinoGov.connect(data.user2Signer).commit(lockTokens.toFixed(0), 55));
+      const [commit4] = await Promise.all(test3);
+      await data.printTxData("commit4", commit4);
+      await data.printBalances();
 
-    console.log("        --- Test 4 - User1 collecting rewards, user2 collecting and committing rewards leaving duration unchanged, user3 collecting and committing rewards and extending duration ---");
-    var test4 = [];
-    test4.push(data.optinoGov.connect(data.user1Signer).collectReward(false, 0));
-    test4.push(data.optinoGov.connect(data.user2Signer).collectReward(true, 0));
-    test4.push(data.optinoGov.connect(data.user3Signer).collectReward(true, 5000));
-    const [collectReward1, collectReward2, collectReward3] = await Promise.all(test4);
-    await data.printTxData("collectReward1", collectReward1);
-    await data.printTxData("collectReward2", collectReward2);
-    await data.printTxData("collectReward3", collectReward3);
-    await data.printBalances();
+      console.log("        --- Test 4 - User1 collecting rewards, user2 collecting and committing rewards leaving duration unchanged, user3 collecting and committing rewards and extending duration ---");
+      const test4 = [];
+      test4.push(data.optinoGov.connect(data.user1Signer).collectReward(false, 0));
+      test4.push(data.optinoGov.connect(data.user2Signer).collectReward(true, 0));
+      test4.push(data.optinoGov.connect(data.user3Signer).collectReward(true, 5000));
+      const [collectReward1, collectReward2, collectReward3] = await Promise.all(test4);
+      await data.printTxData("collectReward1", collectReward1);
+      await data.printTxData("collectReward2", collectReward2);
+      await data.printTxData("collectReward3", collectReward3);
+      await data.printBalances();
 
-    console.log("        --- Test 5 - Owner collecting rewards on behalf of user1 for a % fee ---");
-    data.pause("Waiting", 5);
-    var test5 = [];
-    test5.push(data.optinoGov.collectRewardFor(data.user1));
-    const [collectRewardFor1] = await Promise.all(test5);
-    await data.printTxData("collectRewardFor1", collectRewardFor1);
-    await data.printBalances();
+      console.log("        --- Test 5 - Owner collecting rewards on behalf of user1 for a % fee ---");
+      data.pause("Waiting", 5);
+      const test5 = [];
+      test5.push(data.optinoGov.collectRewardFor(data.user1));
+      const [collectRewardFor1] = await Promise.all(test5);
+      await data.printTxData("collectRewardFor1", collectRewardFor1);
+      await data.printBalances();
 
-    console.log("        --- Test 6 - Owner deposits dividends of 10 ETH and 100 FEE ---");
-    var depositFee1Tokens = new BigNumber("10").shiftedBy(18);
-    var depositFee2Tokens = new BigNumber("100").shiftedBy(18);
-    var test6 = [];
-    test6.push(data.ogdToken.depositDividend(ZERO_ADDRESS, depositFee1Tokens.toFixed(0), { value: depositFee1Tokens.toFixed(0) }));
-    test6.push(data.ogdToken.depositDividend(data.fee0Token.address, depositFee2Tokens.toFixed(0)));
-    const [depositDividendFee1, depositDividendFee2] = await Promise.all(test6);
-    await data.printTxData("depositDividendFee1", depositDividendFee1);
-    await data.printTxData("depositDividendFee2", depositDividendFee2);
-    await data.printBalances();
+      console.log("        --- Test 6 - Owner deposits dividends of 10 ETH and 100 FEE ---");
+      const depositFee1Tokens = new BigNumber("10").shiftedBy(18);
+      const depositFee2Tokens = new BigNumber("100").shiftedBy(18);
+      const test6 = [];
+      test6.push(data.ogdToken.depositDividend(ZERO_ADDRESS, depositFee1Tokens.toFixed(0), { value: depositFee1Tokens.toFixed(0) }));
+      test6.push(data.ogdToken.depositDividend(data.fee0Token.address, depositFee2Tokens.toFixed(0)));
+      const [depositDividendFee1, depositDividendFee2] = await Promise.all(test6);
+      await data.printTxData("depositDividendFee1", depositDividendFee1);
+      await data.printTxData("depositDividendFee2", depositDividendFee2);
+      await data.printBalances();
 
-    console.log("        --- Test 7 - User{1..3} withdraw ETH and FEE dividends ---");
-    var test7 = [];
-    test7.push(data.ogdToken.connect(data.user1Signer).withdrawDividends());
-    test7.push(data.ogdToken.connect(data.user2Signer).withdrawDividends());
-    test7.push(data.ogdToken.connect(data.user3Signer).withdrawDividends());
-    const [withdrawDividends1, withdrawDividends2, withdrawDividends3] = await Promise.all(test7);
-    await data.printTxData("withdrawDividends1", withdrawDividends1);
-    await data.printTxData("withdrawDividends2", withdrawDividends2);
-    await data.printTxData("withdrawDividends3", withdrawDividends3);
-    await data.printBalances();
+      console.log("        --- Test 7 - User{1..3} withdraw ETH and FEE dividends ---");
+      const test7 = [];
+      test7.push(data.ogdToken.connect(data.user1Signer).withdrawDividends());
+      test7.push(data.ogdToken.connect(data.user2Signer).withdrawDividends());
+      test7.push(data.ogdToken.connect(data.user3Signer).withdrawDividends());
+      const [withdrawDividends1, withdrawDividends2, withdrawDividends3] = await Promise.all(test7);
+      await data.printTxData("withdrawDividends1", withdrawDividends1);
+      await data.printTxData("withdrawDividends2", withdrawDividends2);
+      await data.printTxData("withdrawDividends3", withdrawDividends3);
+      await data.printBalances();
 
-    // const user1Fee1Balance = await fee1Token.balanceOf(data.user1);
-    // console.log("user1Fee1Balance: " + user1Fee1Balance);
-    // expect(new BigNumber(user1Fee1Balance.toString()).toFixed(0)).to.equal(new BigNumber("333.333333333333333333").shiftedBy(18).toFixed(0));
+      // const user1Fee1Balance = await fee1Token.balanceOf(data.user1);
+      // console.log("user1Fee1Balance: " + user1Fee1Balance);
+      // expect(new BigNumber(user1Fee1Balance.toString()).toFixed(0)).to.equal(new BigNumber("333.333333333333333333").shiftedBy(18).toFixed(0));
 
-    // const Greeter = await ethers.getContractFactory("Greeter");
-    // const greeter = await Greeter.deploy("Hello, world!");
-    //
-    // await greeter.deployed();
-    // expect(await greeter.greet()).to.equal("Hello, world!");
-    //
-    // await greeter.setGreeting("Hola, mundo!");
-    // // await greeter.connect(data.user1).setGreeting("Hola, mundo!");
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+      // const Greeter = await ethers.getContractFactory("Greeter");
+      // const greeter = await Greeter.deploy("Hello, world!");
+      //
+      // await greeter.deployed();
+      // expect(await greeter.greet()).to.equal("Hello, world!");
+      //
+      // await greeter.setGreeting("Hola, mundo!");
+      // // await greeter.connect(data.user1).setGreeting("Hola, mundo!");
+      // expect(await greeter.greet()).to.equal("Hola, mundo!");
 
-    // await ethers.provider.getLogs({}).then((data) => {
-    //   console.log("getLogs: " + util.inspect(data));
-    // });
+      // await ethers.provider.getLogs({}).then((data) => {
+      //   console.log("getLogs: " + util.inspect(data));
+      // });
+    });
   });
 });
